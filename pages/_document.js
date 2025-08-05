0cf94ad992a42715e42fd76a9ba7e6b326aa99d7
ge79zol <ching-chih.pan@tum.de>
2025-08-05 18:00:57 +0200

    Change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Copy the HTML code from RealFaviconGenerator */}
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
